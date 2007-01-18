4a21a0abe9947680b382f0f64770fff75fdf981f
Sjoerd Simons <sjoerd@luon.net>
2007-01-18 19:06:15 +0000

    Add a test script to run the tests :)
    20070118190615-93b9a-4b5a481ad531a07cd14ddd6ca5bf6faba9b4342a.gz

---
+++ tests/run-test.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+mkdir -p outputs
+
+for x in ./inputs/*.input ; do
+  FILE=$(basename ${x%.input})
+  XMLOUT="outputs/${FILE}.output"
+  PARSEOUT="outputs/${FILE}.parse"
+  ./test-xmpp-connection $x ${PARSEOUT} ${XMLOUT} 
+  if [ ! $? ] ; then
+    echo "FAILED: $x - Test program had non-zero exit status" >&2
+    continue
+  fi
+  xmldiff $x $XMLOUT
+  if [ $? ] ; then
+    echo "FAILED: $x - XML output doesn't match the input" >&2
+    continue
+  fi
+  echo "SUCCESS: $x"
+done
