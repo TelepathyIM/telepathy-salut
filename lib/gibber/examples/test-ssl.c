b924cec988874cf4bf474eb4587fcb3a3cf2b682
Sjoerd Simons <sjoerd@luon.net>
2007-02-19 22:54:20 +0000

    test-ssl: Change from doing old-style ssl to proper tls with starttls
    20070219225420-93b9a-d62d94442c35f48b9fe0484120fec848db930f37.gz

--- lib/gibber/examples/test-ssl.c
+++ lib/gibber/examples/test-ssl.c
@@ -9,20 +9,30 @@
 #include <gibber/gibber-ssl-transport.h>
 #include <gibber/gibber-sasl-auth.h>
 #include <gibber/gibber-xmpp-connection.h>
+#include <gibber/gibber-namespaces.h>
 
+typedef enum {
+  INITIAL,
+  SSL,
+  SSL_DONE,
+  SASL,
+  DONE,
+} State;
+
+
+State state = INITIAL;
 GMainLoop *mainloop;
 GibberXmppConnection *conn;
 const gchar *server;
 const gchar *username;
 const gchar *password;
-gboolean authenticated = FALSE;
 GibberSaslAuth *sasl = NULL;
 GibberSSLTransport *ssl = NULL;
 
 void
 tcp_connected(GibberTCPTransport *tcp, gpointer user_data) {
   printf("TCP transport connected!\n");
-  gibber_ssl_transport_connect(ssl, server, NULL);
+  gibber_xmpp_connection_open(conn, server, NULL, "1.0");
 }
 
 void
@@ -46,6 +56,7 @@ tcp_error(GibberTCPTransport *tcp, guint domain,
 void
 ssl_connected(GibberSSLTransport *ssl, gpointer user_data) {
   printf("SSL transport connected!\n");
+  state = SSL_DONE;
   gibber_xmpp_connection_open(conn, server, NULL, "1.0");
 }
 
@@ -98,6 +109,7 @@ return_str(GibberSaslAuth *auth, gpointer user_data) {
 void
 auth_success(GibberSaslAuth *sasl, gpointer user_data) {
   printf("Authentication successfull!!\n");
+  state = DONE;
   /* Reopen the connection */
   gibber_xmpp_connection_open(conn, server, NULL, "1.0");
 }
@@ -109,27 +121,105 @@ auth_failed(GibberSaslAuth *sasl, GQuark domain,
   g_main_loop_quit(mainloop);
 }
 
+static void
+start_ssl(GibberXmppConnection *connection, GibberXmppStanza *stanza) {
+  GibberTransport *transport;
+  GError *error = NULL;
+
+  if (strcmp(stanza->node->name, "proceed") 
+    || strcmp(gibber_xmpp_node_get_ns(stanza->node), 
+          GIBBER_XMPP_NS_TLS)) {
+    printf("Server doesn't want to start tls");
+    g_main_loop_quit(mainloop);
+  }
+
+  transport = g_object_ref(connection->transport);
+  gibber_xmpp_connection_disengage(connection);
+
+  ssl = gibber_ssl_transport_new(GIBBER_TRANSPORT(transport));
+  gibber_xmpp_connection_engage(connection, GIBBER_TRANSPORT(ssl));
+
+  g_signal_connect(ssl, "connected", 
+                   G_CALLBACK(ssl_connected), NULL);
+  g_signal_connect(ssl, "connecting", 
+                   G_CALLBACK(ssl_connecting), NULL);
+  g_signal_connect(ssl, "disconnected", 
+                   G_CALLBACK(ssl_disconnected), NULL);
+  g_signal_connect(ssl, "error", 
+                   G_CALLBACK(ssl_error), NULL);
+
+  if (!gibber_ssl_transport_connect(ssl, server, &error)) {
+     printf("SSL auth start failed: %s\n", error->message);
+     g_main_loop_quit(mainloop);
+  }
+}
+
+static void 
+negotiate_ssl(GibberXmppConnection *connection, GibberXmppStanza *stanza) {
+  GibberXmppNode *tls;
+  GibberXmppStanza *starttls;
+  if (strcmp(stanza->node->name, "features") 
+    || strcmp(gibber_xmpp_node_get_ns(stanza->node),GIBBER_XMPP_NS_STREAM)) {
+    printf("Didn't receive features stanza\n");
+    g_main_loop_quit(mainloop);
+  } 
+
+  tls = gibber_xmpp_node_get_child_ns(stanza->node, "starttls",
+                                      GIBBER_XMPP_NS_TLS);
+  if (tls == NULL) {
+    printf("Server doesn't support tls\n");
+    g_main_loop_quit(mainloop);
+  }
+
+  starttls = gibber_xmpp_stanza_new("starttls");
+  gibber_xmpp_node_set_ns(starttls->node, GIBBER_XMPP_NS_TLS);
+
+  state = SSL;
+
+  g_assert(gibber_xmpp_connection_send(connection, starttls, NULL));
+  g_object_unref(starttls);
+}
+
+static void
+start_sasl_helper(GibberXmppConnection *connection, GibberXmppStanza *stanza) {
+  GError *error;
+
+  state = SASL;
+  sasl = gibber_sasl_auth_new();
+  g_signal_connect(sasl, "username-requested",
+                    G_CALLBACK(return_str), (gpointer)username);
+  g_signal_connect(sasl, "password-requested",
+                    G_CALLBACK(return_str), (gpointer)password);
+  g_signal_connect(sasl, "authentication-succeeded",
+                    G_CALLBACK(auth_success), NULL);
+  g_signal_connect(sasl, "authentication-failed",
+                    G_CALLBACK(auth_failed), NULL);
+
+  if (!gibber_sasl_auth_authenticate(sasl, server, 
+                                  connection, stanza, TRUE, &error)) {
+     printf("Sasl auth start failed: %s\n", error->message);
+     g_main_loop_quit(mainloop);
+  }
+}
+
 void
 conn_received_stanza(GibberXmppConnection *connection, 
                 GibberXmppStanza *stanza,
                 gpointer user_data) {
-  if (sasl == NULL && !authenticated) { 
-    GError *error;
-    sasl = gibber_sasl_auth_new();
-    g_signal_connect(sasl, "username-requested",
-                      G_CALLBACK(return_str), (gpointer)username);
-    g_signal_connect(sasl, "password-requested",
-                      G_CALLBACK(return_str), (gpointer)password);
-    g_signal_connect(sasl, "authentication-succeeded",
-                      G_CALLBACK(auth_success), NULL);
-    g_signal_connect(sasl, "authentication-failed",
-                      G_CALLBACK(auth_failed), NULL);
-
-    if (!gibber_sasl_auth_authenticate(sasl, server, 
-                                  connection, stanza, TRUE, &error)) {
-      printf("Sasl auth start failed: %s\n", error->message);
-      g_main_loop_quit(mainloop);
-    }
+  
+  switch (state ) {
+    case INITIAL:
+      negotiate_ssl(connection, stanza);
+      break;
+    case SSL:
+      start_ssl(connection, stanza);
+      break;
+    case SSL_DONE:
+      start_sasl_helper(connection, stanza);
+      break;
+    case SASL:
+    case DONE:
+      break;
   }
 }
 
@@ -154,18 +244,8 @@ main(int argc, char **argv) {
   g_signal_connect(tcp, "error", 
                    G_CALLBACK(tcp_error), NULL);
 
-  ssl = gibber_ssl_transport_new(GIBBER_TRANSPORT(tcp));
 
-  g_signal_connect(ssl, "connected", 
-                   G_CALLBACK(ssl_connected), NULL);
-  g_signal_connect(ssl, "connecting", 
-                   G_CALLBACK(ssl_connecting), NULL);
-  g_signal_connect(ssl, "disconnected", 
-                   G_CALLBACK(ssl_disconnected), NULL);
-  g_signal_connect(ssl, "error", 
-                   G_CALLBACK(ssl_error), NULL);
-
-  conn = gibber_xmpp_connection_new(GIBBER_TRANSPORT(ssl));
+  conn = gibber_xmpp_connection_new(GIBBER_TRANSPORT(tcp));
   g_signal_connect(conn, "parse-error",
       G_CALLBACK(conn_parse_error), NULL);
   g_signal_connect(conn, "stream-opened",
