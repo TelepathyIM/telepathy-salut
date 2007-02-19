ab25c79ab944f704695b5d028345bcb05fb72e7c
Sjoerd Simons <sjoerd@luon.net>
2007-02-19 22:52:17 +0000

    gibber-namespaces.h: Add TLS namespace
    20070219225217-93b9a-a939a12c3ca9cc6a087d08e9e3b045b0a2568f60.gz

--- lib/gibber/gibber-namespaces.h
+++ lib/gibber/gibber-namespaces.h
@@ -2,6 +2,9 @@
 #define GIBBER_XMPP_NS_STREAM \
   (const gchar *)"http://etherx.jabber.org/streams"
 
+#define GIBBER_XMPP_NS_TLS \
+  (const gchar *)"urn:ietf:params:xml:ns:xmpp-tls"
+
 #define GIBBER_XMPP_NS_SASL_AUTH \
   (const gchar *)"urn:ietf:params:xml:ns:xmpp-sasl"
 
