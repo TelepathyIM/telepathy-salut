22a04f32bae2446bd7ca32386592b386cbc5fe38
Sjoerd Simons <sjoerd@luon.net>
2007-03-16 21:26:30 +0000

    Add xhtml-im and w3c xhtml namespace
    20070316212630-93b9a-bcdbd042585561b0f20076b5e7f5f1c41c1e2867.gz

--- lib/gibber/gibber-namespaces.h
+++ lib/gibber/gibber-namespaces.h
@@ -8,3 +8,8 @@
 #define GIBBER_XMPP_NS_SASL_AUTH \
   (const gchar *)"urn:ietf:params:xml:ns:xmpp-sasl"
 
+#define GIBBER_XMPP_NS_XHTML_IM \
+  (const gchar *)"http://jabber.org/protocol/xhtml-im"
+
+#define GIBBER_W3C_NS_XHTML \
+  (const gchar *)"http://www.w3.org/1999/xhtml"
