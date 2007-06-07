e35d261bd00ca8ce0af9a5d4dbb2bea08e1bf9f9
Sjoerd Simons <sjoerd@luon.net>
2007-06-07 07:40:34 +0000

    Add a fatal timeout, send the packets in a 100 ms interval instead of as fast as possible
    20070607074034-93b9a-4eeff4f737805d4e7d9eee615fdb553231c0e592.gz

--- tests/simplemeshtest.py
+++ tests/simplemeshtest.py
@@ -4,7 +4,7 @@ from twisted.internet import reactor
 from mesh import Mesh
 import sys
 
-NUMNODES = 5
+NUMNODES = 10
 NUMPACKETS = 10
 
 
@@ -17,6 +17,7 @@ class TestMesh(Mesh):
   done = 0
 
   def gotOutput(self, node, data):
+    print node.name + " - " + data.rstrip()
     if self.expected.get(node) == None:
       self.expected[node] = int(data)
 
@@ -43,13 +44,22 @@ for x in xrange(0, NUMNODES):
 
 # Connect all nodes to all others. 1024 bytes/s bandwidth, 50ms delay and 50%
 # packet loss.. (bandwidth and delay aren't implemented just yet)
-m.connect_full(1024, 50, 0.50)
+m.connect_full(1024, 50, 0.30)
 
 firstnode = nodes[0]
 for x in xrange(0, NUMPACKETS):
-  firstnode.pushInput(str(x) + "\n")
+  reactor.callLater(0.1 * x, (lambda y: firstnode.pushInput(str(y) + "\n")), x)
+
+def timeout():
+  global success
+  print "TIMEOUT!"
+  success = False
+  reactor.crash()
+
+reactor.callLater(60, timeout)
 
 reactor.run()
+
 
 if not success:
   print "FAILED"
