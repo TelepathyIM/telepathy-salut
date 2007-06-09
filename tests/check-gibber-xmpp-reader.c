89aa2fe03d823ee171e2f9d33fe39af62e5be303
Emanuele Aina <em@nerd.ocracy.org>
2007-06-09 13:44:05 +0000

    Tested the parsing of a simple XMPP message
    20070609134405-f974e-cc262423822f79edefa81da64bfd39c4c6ebeb7d.gz

--- tests/check-gibber-xmpp-reader.c
+++ tests/check-gibber-xmpp-reader.c
@@ -15,10 +15,28 @@ START_TEST (test_instantiation)
 }
 END_TEST
 
+START_TEST (test_simple_message)
+{
+  GibberXmppReader *reader;
+  gchar *data;
+  gsize length;
+  gboolean valid;
+
+  g_type_init();
+  reader = gibber_xmpp_reader_new_no_stream ();
+  fail_unless (g_file_get_contents ("inputs/simple-message.input",
+                                    &data, &length, NULL));
+
+  valid = gibber_xmpp_reader_push (reader, (guint8 *)data, length, NULL);
+  fail_unless (valid);
+}
+END_TEST
+
 TCase *
 make_gibber_xmpp_reader_tcase (void)
 {
     TCase *tc = tcase_create ("XMPP Reader");
     tcase_add_test (tc, test_instantiation);
+    tcase_add_test (tc, test_simple_message);
     return tc;
 }
