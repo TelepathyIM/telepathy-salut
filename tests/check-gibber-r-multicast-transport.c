e2f646ee01f90821fcdc1b936b97ecf1555bb4a7
Sjoerd Simons <sjoerd.simons@collabora.co.uk>
2007-06-18 13:29:07 +0000

    Add test to see if dependencies are correctly add to packages
    20070618132907-b58c9-39054681be70d5c7d9b60dc74bce89c16be65f1b.gz

--- tests/check-gibber-r-multicast-transport.c
+++ tests/check-gibber-r-multicast-transport.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
 #include <gibber/gibber-r-multicast-transport.h>
 #include <gibber/gibber-r-multicast-packet.h>
@@ -29,6 +31,116 @@ create_rmulticast_transport (TestTransport **testtransport, const gchar *name,
   return rmtransport;
 }
 
+/* test depends test */
+struct {
+  gchar *name;
+  guint32 packet_id;
+  gboolean seen;
+} senders[] = {
+                { "test0",     0xff, FALSE },
+                { "test1",   0xffff, FALSE },
+                { "test2", 0xffffff, FALSE },
+                { "test3", 0xaaaaaa, FALSE },
+                { "test4", 0xabcdab, FALSE },
+                { NULL, 0, FALSE }
+              };
+
+gboolean
+depends_send_hook(GibberTransport *transport, const guint8 *data,
+          gsize length, GError **error, gpointer user_data) {
+  GibberRMulticastPacket *packet;
+  int i;
+  GList *l;
+
+  packet = gibber_r_multicast_packet_parse (data, length, NULL);
+  fail_unless (packet != NULL);
+
+  if (packet->type != PACKET_TYPE_DATA) {
+    goto out;
+  }
+
+  fail_unless(g_list_length(packet->receivers) > 0);
+
+  for (l = packet->receivers; l != NULL; l = g_list_next(l)) {
+    for (i = 0; senders[i].name != NULL ; i++) {
+      GibberRMulticastReceiver *r = (GibberRMulticastReceiver *)l->data;
+      if (strcmp(senders[i].name, r->name) == 0) {
+        fail_unless(senders[i].seen == FALSE);
+        fail_unless(senders[i].packet_id == r->packet_id);
+        senders[i].seen = TRUE;
+        break;
+      }
+    }
+    fail_unless(senders[i].name != NULL);
+  }
+
+  for (i = 0; senders[i].name != NULL ; i++) {
+    fail_unless(senders[i].seen, "Not all senders in depends");
+  }
+
+  g_object_unref(packet);
+  g_main_loop_quit(loop);
+out:
+  return TRUE;
+}
+
+static gboolean
+depends_send_test_data(gpointer data) {
+  GibberRMulticastTransport *t = GIBBER_R_MULTICAST_TRANSPORT(data);
+  guint8 testdata[] = { 1, 2, 3 };
+
+  fail_unless(gibber_transport_send(GIBBER_TRANSPORT(t), testdata,
+    3, NULL));
+
+  return FALSE;
+}
+
+
+START_TEST (test_depends)
+{
+  GibberRMulticastTransport *rmtransport;
+  TestTransport *testtransport;
+  int i;
+
+  g_type_init();
+
+  loop = g_main_loop_new (NULL, FALSE);
+
+  rmtransport = create_rmulticast_transport(&testtransport, "test123",
+       depends_send_hook);
+
+  fail_unless(gibber_r_multicast_transport_connect(rmtransport, FALSE, NULL));
+
+  /* First input some data packets, so the transport is forced to generate
+   * dependency info */
+  for (i = 0 ; senders[i].name != NULL; i++) {
+    GibberRMulticastPacket *packet;
+    guint8 *data;
+    gsize size;
+    packet = gibber_r_multicast_packet_new( PACKET_TYPE_DATA, senders[i].name,
+        senders[i].packet_id,
+        GIBBER_TRANSPORT(testtransport)->max_packet_size);
+
+    gibber_r_multicast_packet_set_part(packet, 0, 1);
+    data = gibber_r_multicast_packet_get_raw_data(packet, &size);
+    test_transport_write(testtransport, data, size);
+    g_object_unref(packet);
+  }
+
+  /* Wait more then 200 ms, so all senders can get go to running */
+  g_timeout_add(300, depends_send_test_data, rmtransport);
+
+  g_main_loop_run(loop);
+  g_main_loop_unref(loop);
+
+  for (i = 0 ; senders[i].name != NULL; i++) {
+    fail_unless(senders[i].seen);
+  }
+}
+END_TEST
+
+
+/* test fragmentation testing */
 gboolean
 fragmentation_send_hook(GibberTransport *transport, const guint8 *data,
           gsize length, GError **error, gpointer user_data)
@@ -84,7 +196,7 @@ START_TEST (test_fragmentation)
 
   loop = g_main_loop_new (NULL, FALSE);
 
-  rmtransport = create_rmulticast_transport(NULL, "test123", 
+  rmtransport = create_rmulticast_transport(NULL, "test123",
        fragmentation_send_hook);
 
   fail_unless(gibber_r_multicast_transport_connect(rmtransport, FALSE, NULL));
@@ -93,6 +205,7 @@ START_TEST (test_fragmentation)
       (guint8 *)testdata, TEST_DATA_SIZE, NULL));
 
   g_main_loop_run(loop);
+  g_main_loop_unref(loop);
 }
 END_TEST
 
@@ -101,6 +214,7 @@ make_gibber_r_multicast_transport_tcase (void)
 {
   TCase *tc = tcase_create("Gibber R Multicast transport");
   tcase_add_test (tc, test_fragmentation);
+  tcase_add_test (tc, test_depends);
 
   return tc;
 }
