222e6aab5866b842b68e7133516838e9f99e52ae
Emanuele Aina <em@nerd.ocracy.org>
2007-06-27 13:30:58 +0000

    Make check-main fail again if there are failing tests
    20070627133058-f974e-9acf9c459d46dfb8c164e3838be3a1a1c7322edd.gz

--- tests/check-main.c
+++ tests/check-main.c
@@ -44,7 +44,7 @@ make_salut_suite (void)
 int
 main (void)
 {
-    int number_failed;
+    int number_failed = 0;
     Suite *s;
     SRunner *sr;
 
@@ -54,15 +54,14 @@ main (void)
     s = make_gibber_suite ();
     sr = srunner_create (s);
     srunner_run_all (sr, CK_NORMAL);
-    number_failed = srunner_ntests_failed (sr);
+    number_failed += srunner_ntests_failed (sr);
     srunner_free (sr);
 
     s = make_salut_suite ();
     sr = srunner_create (s);
     srunner_run_all (sr, CK_NORMAL);
-    number_failed = srunner_ntests_failed (sr);
+    number_failed += srunner_ntests_failed (sr);
     srunner_free (sr);
-
 
     return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
