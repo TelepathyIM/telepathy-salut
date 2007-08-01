fc1061b37f2d64e6b03730f9801ac2d1ea70fbdb
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2007-08-01 09:54:21 +0000

    add gibber-bytestream-iface.[ch]
    20070801095421-7fe3f-7d8ba7dfed7aab122e349d8291d2b07abcd2c849.gz

---
+++ lib/gibber/gibber-bytestream-iface.h
@@ -0,0 +1,91 @@
+/*
+ * bytestream-iface.h - Header for GibberBytestream interface
+ * Copyright (C) 2007 Collabora Ltd.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef __GIBBER_BYTESTREAM_IFACE_H__
+#define __GIBBER_BYTESTREAM_IFACE_H__
+
+#include <glib-object.h>
+
+#include "gibber-xmpp-stanza.h"
+
+G_BEGIN_DECLS
+
+typedef enum
+{
+  /* Received a SI request, response not yet sent */
+  GIBBER_BYTESTREAM_STATE_LOCAL_PENDING = 0,
+  /* We accepted SI request.
+   * bytestream specific init steps not yet performed */
+  GIBBER_BYTESTREAM_STATE_ACCEPTED,
+  /* Remote contact accepted the SI request.
+   * bytestream specific initiation started */
+  GIBBER_BYTESTREAM_STATE_INITIATING,
+  /* Bytestream open */
+  GIBBER_BYTESTREAM_STATE_OPEN,
+  GIBBER_BYTESTREAM_STATE_CLOSED,
+  LAST_GIBBER_BYTESTREAM_STATE,
+} GibberBytestreamState;
+
+typedef struct _GibberBytestreamIface GibberBytestreamIface;
+typedef struct _GibberBytestreamIfaceClass GibberBytestreamIfaceClass;
+
+struct _GibberBytestreamIfaceClass {
+  GTypeInterface parent;
+
+  gboolean (*initiation) (GibberBytestreamIface *bytestream);
+  gboolean (*send) (GibberBytestreamIface *bytestream, guint len, gchar *data);
+  void (*close) (GibberBytestreamIface *bytestream);
+  void (*accept) (GibberBytestreamIface *bytestream);
+  const gchar * (*get_protocol) (GibberBytestreamIface *bytestream);
+};
+
+GType gibber_bytestream_iface_get_type (void);
+
+/* TYPE MACROS */
+#define GIBBER_TYPE_BYTESTREAM_IFACE \
+  (gibber_bytestream_iface_get_type ())
+#define GIBBER_BYTESTREAM_IFACE(obj) \
+  (G_TYPE_CHECK_INSTANCE_CAST((obj), GIBBER_TYPE_BYTESTREAM_IFACE, \
+                              GibberBytestreamIface))
+#define GIBBER_IS_BYTESTREAM_IFACE(obj) \
+  (G_TYPE_CHECK_INSTANCE_TYPE((obj), GIBBER_TYPE_BYTESTREAM_IFACE))
+#define GIBBER_BYTESTREAM_IFACE_GET_CLASS(obj) \
+  (G_TYPE_INSTANCE_GET_INTERFACE ((obj), GIBBER_TYPE_BYTESTREAM_IFACE,\
+                              GibberBytestreamIfaceClass))
+
+gboolean
+gibber_bytestream_iface_initiation (GibberBytestreamIface *bytestream);
+
+gboolean
+gibber_bytestream_iface_send (GibberBytestreamIface *bytestream, guint len,
+   gchar *data);
+
+void
+gibber_bytestream_iface_close (GibberBytestreamIface *bytestream);
+
+void
+gibber_bytestream_iface_accept (GibberBytestreamIface *bytestream);
+
+const gchar *
+gibber_bytestream_iface_get_protocol (GibberBytestreamIface *bytestream);
+
+
+G_END_DECLS
+
+#endif /* #ifndef __GIBBER_BYTESTREAM_IFACE_H__ */
