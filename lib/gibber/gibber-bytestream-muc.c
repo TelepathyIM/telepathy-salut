475707c45714eee18c427380c8be747c5c708a87
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2007-09-04 11:35:57 +0000

    add gibber-bytestream-muc.[ch]
    20070904113557-7fe3f-3cbf2c73702d23bcd3d1b27b9ff225d4dd23f3da.gz

---
+++ lib/gibber/gibber-bytestream-muc.c
@@ -0,0 +1,399 @@
+/*
+ * gibber-bytestream-ibb.c - Source for GibberBytestreamMuc
+ * Copyright (C) 2007 Collabora Ltd.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "gibber-bytestream-muc.h"
+
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include <glib.h>
+
+#include "gibber-bytestream-iface.h"
+#include "gibber-muc-connection.h"
+#include "gibber-linklocal-transport.h"
+
+#define DEBUG_FLAG DEBUG_BYTESTREAM
+#include "gibber-debug.h"
+
+#include "signals-marshal.h"
+
+static void
+bytestream_iface_init (gpointer g_iface, gpointer iface_data);
+
+G_DEFINE_TYPE_WITH_CODE (GibberBytestreamMuc, gibber_bytestream_muc,
+    G_TYPE_OBJECT,
+    G_IMPLEMENT_INTERFACE (GIBBER_TYPE_BYTESTREAM_IFACE,
+      bytestream_iface_init));
+
+/* signals */
+enum
+{
+  DATA_RECEIVED,
+  STATE_CHANGED,
+  LAST_SIGNAL
+};
+
+static guint signals[LAST_SIGNAL] = {0};
+
+/* properties */
+enum
+{
+  PROP_MUC_CONNECTION = 1,
+  PROP_SELF_ID,
+  PROP_PEER_ID,
+  PROP_STREAM_ID,
+  PROP_STATE,
+  LAST_PROPERTY
+};
+
+typedef struct _GibberBytestreamMucPrivate GibberBytestreamMucPrivate;
+struct _GibberBytestreamMucPrivate
+{
+  GibberMucConnection *muc_connection;
+  gchar *self_id;
+  gchar *peer_id;
+  gchar *stream_id;
+  gchar *stream_init_id;
+  GibberBytestreamState state;
+  guint8 stream_id_multicast;
+
+  gboolean dispose_has_run;
+};
+
+#define GIBBER_BYTESTREAM_MUC_GET_PRIVATE(obj) \
+    ((GibberBytestreamMucPrivate *) obj->priv)
+
+static void
+gibber_bytestream_muc_init (GibberBytestreamMuc *self)
+{
+  GibberBytestreamMucPrivate *priv = G_TYPE_INSTANCE_GET_PRIVATE (self,
+      GIBBER_TYPE_BYTESTREAM_MUC, GibberBytestreamMucPrivate);
+
+  self->priv = priv;
+
+  priv->muc_connection = NULL;
+  priv->self_id = NULL;
+  priv->peer_id = NULL;
+
+  priv->dispose_has_run = FALSE;
+}
+
+static void
+muc_connection_received_data_cb (GibberMucConnection *muc_connection,
+                                 const gchar *sender,
+                                 guint8 stream_id,
+                                 const guint8 *data,
+                                 gsize length,
+                                 GibberBytestreamMuc *self)
+{
+  GibberBytestreamMucPrivate *priv = GIBBER_BYTESTREAM_MUC_GET_PRIVATE (self);
+  GString *str;
+
+  if (priv->stream_id_multicast != stream_id)
+    return;
+
+  str = g_string_new_len ((const gchar *) data, length);
+  g_signal_emit (G_OBJECT (self), signals[DATA_RECEIVED], 0, sender,
+    str);
+
+  g_string_free (str, TRUE);
+}
+
+static void
+gibber_bytestream_muc_dispose (GObject *object)
+{
+  GibberBytestreamMuc *self = GIBBER_BYTESTREAM_MUC (object);
+  GibberBytestreamMucPrivate *priv = GIBBER_BYTESTREAM_MUC_GET_PRIVATE (self);
+
+  if (priv->dispose_has_run)
+    return;
+
+  priv->dispose_has_run = TRUE;
+
+  if (priv->state != GIBBER_BYTESTREAM_STATE_CLOSED)
+    {
+      gibber_bytestream_iface_close (GIBBER_BYTESTREAM_IFACE (self));
+    }
+
+  G_OBJECT_CLASS (gibber_bytestream_muc_parent_class)->dispose (object);
+}
+
+static void
+gibber_bytestream_muc_finalize (GObject *object)
+{
+  GibberBytestreamMuc *self = GIBBER_BYTESTREAM_MUC (object);
+  GibberBytestreamMucPrivate *priv = GIBBER_BYTESTREAM_MUC_GET_PRIVATE (self);
+
+  g_free (priv->stream_id);
+  g_free (priv->stream_init_id);
+  g_free (priv->self_id);
+  g_free (priv->peer_id);
+
+  G_OBJECT_CLASS (gibber_bytestream_muc_parent_class)->finalize (object);
+}
+
+static void
+gibber_bytestream_muc_get_property (GObject *object,
+                                    guint property_id,
+                                    GValue *value,
+                                    GParamSpec *pspec)
+{
+  GibberBytestreamMuc *self = GIBBER_BYTESTREAM_MUC (object);
+  GibberBytestreamMucPrivate *priv = GIBBER_BYTESTREAM_MUC_GET_PRIVATE (self);
+
+  switch (property_id)
+    {
+      case PROP_MUC_CONNECTION:
+        g_value_set_object (value, priv->muc_connection);
+        break;
+      case PROP_SELF_ID:
+        g_value_set_string (value, priv->self_id);
+        break;
+      case PROP_PEER_ID:
+        g_value_set_string (value, priv->peer_id);
+        break;
+      case PROP_STREAM_ID:
+        g_value_set_string (value, priv->stream_id);
+        break;
+      case PROP_STATE:
+        g_value_set_uint (value, priv->state);
+        break;
+      default:
+        G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+        break;
+    }
+}
+
+static void
+gibber_bytestream_muc_set_property (GObject *object,
+                                    guint property_id,
+                                    const GValue *value,
+                                    GParamSpec *pspec)
+{
+  GibberBytestreamMuc *self = GIBBER_BYTESTREAM_MUC (object);
+  GibberBytestreamMucPrivate *priv = GIBBER_BYTESTREAM_MUC_GET_PRIVATE (self);
+
+  switch (property_id)
+    {
+      case PROP_MUC_CONNECTION:
+        priv->muc_connection = g_value_get_object (value);
+        if (priv->muc_connection != NULL)
+          {
+            g_signal_connect (priv->muc_connection, "received-data",
+                G_CALLBACK (muc_connection_received_data_cb), self);
+          }
+        break;
+      case PROP_SELF_ID:
+        g_free (priv->self_id);
+        priv->self_id = g_value_dup_string (value);
+        break;
+      case PROP_PEER_ID:
+        g_free (priv->peer_id);
+        priv->peer_id = g_value_dup_string (value);
+        break;
+      case PROP_STREAM_ID:
+        g_free (priv->stream_id);
+        priv->stream_id = g_value_dup_string (value);
+        break;
+      case PROP_STATE:
+        if (priv->state != g_value_get_uint (value))
+            {
+              priv->state = g_value_get_uint (value);
+              g_signal_emit (object, signals[STATE_CHANGED], 0, priv->state);
+            }
+        break;
+      default:
+        G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+        break;
+    }
+}
+
+static GObject *
+gibber_bytestream_muc_constructor (GType type,
+                                   guint n_props,
+                                   GObjectConstructParam *props)
+{
+  GObject *obj;
+  GibberBytestreamMucPrivate *priv;
+
+  obj = G_OBJECT_CLASS (gibber_bytestream_muc_parent_class)->
+           constructor (type, n_props, props);
+
+  priv = GIBBER_BYTESTREAM_MUC_GET_PRIVATE (GIBBER_BYTESTREAM_MUC (obj));
+
+  /* We can't be a private *and* a muc bytestream */
+  g_assert (priv->muc_connection != NULL);
+  g_assert (priv->self_id != NULL);
+  g_assert (priv->peer_id != NULL);
+  g_assert (priv->stream_id != NULL);
+
+  priv->stream_id_multicast = (guint8) atoi (priv->stream_id);
+
+  return obj;
+}
+
+static void
+gibber_bytestream_muc_class_init (
+    GibberBytestreamMucClass *gibber_bytestream_muc_class)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (gibber_bytestream_muc_class);
+  GParamSpec *param_spec;
+
+  g_type_class_add_private (gibber_bytestream_muc_class,
+      sizeof (GibberBytestreamMucPrivate));
+
+  object_class->dispose = gibber_bytestream_muc_dispose;
+  object_class->finalize = gibber_bytestream_muc_finalize;
+
+  object_class->get_property = gibber_bytestream_muc_get_property;
+  object_class->set_property = gibber_bytestream_muc_set_property;
+  object_class->constructor = gibber_bytestream_muc_constructor;
+
+  g_object_class_override_property (object_class, PROP_SELF_ID,
+      "self-id");
+  g_object_class_override_property (object_class, PROP_PEER_ID,
+      "peer-id");
+  g_object_class_override_property (object_class, PROP_STREAM_ID,
+      "stream-id");
+  g_object_class_override_property (object_class, PROP_STATE,
+      "state");
+
+  param_spec = g_param_spec_object (
+      "muc-connection",
+      "GibberMucConnection object",
+      "Gibber MUC connection object used for communication by this "
+      "bytestream if it's a muc one",
+      GIBBER_TYPE_MUC_CONNECTION,
+      G_PARAM_CONSTRUCT_ONLY |
+      G_PARAM_READWRITE |
+      G_PARAM_STATIC_NAME |
+      G_PARAM_STATIC_NICK |
+      G_PARAM_STATIC_BLURB);
+  g_object_class_install_property (object_class, PROP_MUC_CONNECTION,
+      param_spec);
+
+  signals[DATA_RECEIVED] =
+    g_signal_new ("data-received",
+                  G_OBJECT_CLASS_TYPE (gibber_bytestream_muc_class),
+                  G_SIGNAL_RUN_LAST | G_SIGNAL_DETAILED,
+                  0,
+                  NULL, NULL,
+                  _gibber_signals_marshal_VOID__STRING_POINTER,
+                  G_TYPE_NONE, 2, G_TYPE_STRING, G_TYPE_POINTER);
+
+  signals[STATE_CHANGED] =
+    g_signal_new ("state-changed",
+                  G_OBJECT_CLASS_TYPE (gibber_bytestream_muc_class),
+                  G_SIGNAL_RUN_LAST | G_SIGNAL_DETAILED,
+                  0,
+                  NULL, NULL,
+                  g_cclosure_marshal_VOID__UINT,
+                  G_TYPE_NONE, 1, G_TYPE_UINT);
+}
+
+/*
+ * gibber_bytestream_muc_send
+ *
+ * Implements gibber_bytestream_iface_send on GibberBytestreamIface
+ */
+static gboolean
+gibber_bytestream_muc_send (GibberBytestreamIface *bytestream,
+                            guint len,
+                            const gchar *str)
+{
+  GibberBytestreamMuc *self = GIBBER_BYTESTREAM_MUC (bytestream);
+  GibberBytestreamMucPrivate *priv = GIBBER_BYTESTREAM_MUC_GET_PRIVATE (self);
+  GError *error = NULL;
+
+  if (!gibber_muc_connection_send_raw (priv->muc_connection, priv->stream_id_multicast,
+        (const guint8 *) str, len, &error))
+    {
+      DEBUG ("send failed: %s", error->message);
+      g_error_free (error);
+      return FALSE;
+    }
+
+  return TRUE;
+}
+
+/*
+ * gibber_bytestream_muc_accept
+ *
+ * Implements gibber_bytestream_iface_accept on GibberBytestreamIface
+ */
+static void
+gibber_bytestream_muc_accept (GibberBytestreamIface *bytestream)
+{
+  /* Don't have to accept a muc bytestream */
+}
+
+/*
+ * gibber_bytestream_muc_close
+ *
+ * Implements gibber_bytestream_iface_close on GibberBytestreamIface
+ */
+static void
+gibber_bytestream_muc_close (GibberBytestreamIface *bytestream)
+{
+  GibberBytestreamMuc *self = GIBBER_BYTESTREAM_MUC (bytestream);
+  GibberBytestreamMucPrivate *priv = GIBBER_BYTESTREAM_MUC_GET_PRIVATE (self);
+
+  if (priv->state == GIBBER_BYTESTREAM_STATE_CLOSED)
+     /* bytestream already closed, do nothing */
+     return;
+
+  g_object_set (self, "state", GIBBER_BYTESTREAM_STATE_CLOSED, NULL);
+}
+
+/*
+ * gibber_bytestream_muc_initiate
+ *
+ * Implements gibber_bytestream_iface_initiate on GibberBytestreamIface
+ */
+gboolean
+gibber_bytestream_muc_initiate (GibberBytestreamIface *bytestream)
+{
+  /* Nothing to do */
+  return TRUE;
+}
+
+/*
+ * gibber_bytestream_muc_get_protocol
+ *
+ * Implements gibber_bytestream_iface_get_protocol on GibberBytestreamIface
+ */
+static const gchar *
+gibber_bytestream_muc_get_protocol (GibberBytestreamIface *bytestream)
+{
+  return "rmulticast";
+}
+
+static void
+bytestream_iface_init (gpointer g_iface,
+                       gpointer iface_data)
+{
+  GibberBytestreamIfaceClass *klass = (GibberBytestreamIfaceClass *) g_iface;
+
+  klass->initiate = gibber_bytestream_muc_initiate;
+  klass->send = gibber_bytestream_muc_send;
+  klass->close = gibber_bytestream_muc_close;
+  klass->accept = gibber_bytestream_muc_accept;
+  klass->get_protocol = gibber_bytestream_muc_get_protocol;
+}
---
+++ lib/gibber/gibber-bytestream-muc.h
@@ -0,0 +1,63 @@
+/*
+ * gibber-bytestream-ibb.h - Header for GibberBytestreamMuc
+ * Copyright (C) 2007 Collabora Ltd.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef __GIBBER_BYTESTREAM_MUC_H__
+#define __GIBBER_BYTESTREAM_MUC_H__
+
+#include <glib-object.h>
+
+#include "gibber-bytestream-iface.h"
+
+G_BEGIN_DECLS
+
+typedef struct _GibberBytestreamMuc GibberBytestreamMuc;
+typedef struct _GibberBytestreamMucClass GibberBytestreamMucClass;
+
+struct _GibberBytestreamMucClass {
+  GObjectClass parent_class;
+};
+
+struct _GibberBytestreamMuc {
+  GObject parent;
+
+  gpointer priv;
+};
+
+GType gibber_bytestream_muc_get_type (void);
+
+/* TYPE MACROS */
+#define GIBBER_TYPE_BYTESTREAM_MUC \
+  (gibber_bytestream_muc_get_type ())
+#define GIBBER_BYTESTREAM_MUC(obj) \
+  (G_TYPE_CHECK_INSTANCE_CAST((obj), GIBBER_TYPE_BYTESTREAM_MUC,\
+                              GibberBytestreamMuc))
+#define GIBBER_BYTESTREAM_MUC_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_CAST((klass), GIBBER_TYPE_BYTESTREAM_MUC,\
+                           GibberBytestreamMucClass))
+#define GIBBER_IS_BYTESTREAM_MUC(obj) \
+  (G_TYPE_CHECK_INSTANCE_TYPE((obj), GIBBER_TYPE_BYTESTREAM_MUC))
+#define GIBBER_IS_BYTESTREAM_MUC_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_TYPE((klass), GIBBER_TYPE_BYTESTREAM_MUC))
+#define GIBBER_BYTESTREAM_MUC_GET_CLASS(obj) \
+  (G_TYPE_INSTANCE_GET_CLASS ((obj), GIBBER_TYPE_BYTESTREAM_MUC,\
+                              GibberBytestreamMucClass))
+
+G_END_DECLS
+
+#endif /* #ifndef __GIBBER_BYTESTREAM_MUC_H__ */
