3086e9264a860253ff8eabaee6af4093ac1e5f1c
Sjoerd Simons <sjoerd.simons@collabora.co.uk>
2008-03-20 11:34:52 +0000

    continous-failure.py: Update test to send messages on a regular basis to see check if data is still send after a reset
    20080320113452-b58c9-08851cf6347ebf8cc7dd2e01a5c72fed36125220.gz

--- tests/continous-failure.py
+++ tests/continous-failure.py
@@ -12,24 +12,40 @@ DELAY = 0.1
 m = Mesh()
 
 class TestMeshNode(MeshNode):
+  def __init__(self, name, mesh):
+    MeshNode.__init__(self, name, mesh)
+    self.count = 0
+
+  def node_connected (self):
+    MeshNode.node_connected(self)
+    reactor.callLater (0.5, self.push)
 
   def newNode (self, data):
     MeshNode.newNode (self, data)
     print data + " joined"
-    self.fail(data)
 
   def leftNode (self, data):
     MeshNode.leftNode (self, data)
     print data + " left"
 
+  def push (self):
+    reactor.callLater (2, self.push)
+    self.pushInput (self.name + " " + str(self.count))
+    self.count += 1
+
+class FailMeshNode(TestMeshNode):
+  def newNode (self, data):
+    TestMeshNode.newNode (self, data)
+    reactor.callLater (5, self.fail, "node")
+
 n = TestMeshNode("node", m)
 m.addMeshNode(n)
 
-failnode = MeshNode("failnode", m)
+failnode = FailMeshNode("failnode", m)
 m.addMeshNode(failnode)
 
 # Connect all nodes to all others. 1024 bytes/s bandwidth, 50ms delay and 0%
 # packet loss.. (bandwidth and delay aren't implemented just yet)
-m.connect_full(1024, 50, 0.30)
+m.connect_full(1024, 50, 0.00)
 
 reactor.run()
