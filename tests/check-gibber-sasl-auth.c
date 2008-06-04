64015f617a0b836579e74152b8bcbfe8f7e6ff99
Sjoerd Simons <sjoerd@luon.net>
2008-06-04 11:31:58 +0000

    Coding style updates
    20080604113158-93b9a-6c2078534b174e0730dfb45389ee2fbca53827a8.gz

--- tests/check-gibber-sasl-auth.c
+++ tests/check-gibber-sasl-auth.c
@@ -33,134 +33,145 @@ test_t *current_test = NULL;
 GError *error = NULL;
 
 static void
-got_error(GQuark domain, int code, gchar *message) {
-  g_set_error(&error, domain, code, message);
+got_error (GQuark domain, int code, gchar *message)
+{
+  g_set_error (&error, domain, code, message);
   run_done = TRUE;
-  g_main_loop_quit(mainloop);
+  g_main_loop_quit (mainloop);
 }
 
 gboolean
-send_hook(GibberTransport *transport, const guint8 *data,
-   gsize length, GError **error, gpointer user_data) {
+send_hook (GibberTransport *transport, const guint8 *data,
+   gsize length, GError **error, gpointer user_data)
+{
   GibberTransport *target =
      (servertransport == transport) ? clienttransport : servertransport;
 
-  test_transport_write(TEST_TRANSPORT(target), data, length);
+  test_transport_write (TEST_TRANSPORT (target), data, length);
   return TRUE;
 }
 
 gchar *
-return_str(GibberSaslAuth *auth, gpointer user_data) {
-  return g_strdup(user_data);
+return_str (GibberSaslAuth *auth, gpointer user_data)
+{
+  return g_strdup (user_data);
 }
 
 void
-auth_success(GibberSaslAuth *sasl, gpointer user_data) {
+auth_success (GibberSaslAuth *sasl, gpointer user_data)
+{
   authenticated = TRUE;
   /* Reopen the connection */
-  gibber_xmpp_connection_restart(conn);
-  gibber_xmpp_connection_open(conn, servername, NULL, "1.0");
+  gibber_xmpp_connection_restart (conn);
+  gibber_xmpp_connection_open (conn, servername, NULL, "1.0");
 }
 
 void
-auth_failed(GibberSaslAuth *sasl, GQuark domain,
-    int code, gchar *message, gpointer user_data) {
-  got_error(domain, code, message);
+auth_failed (GibberSaslAuth *sasl, GQuark domain,
+    int code, gchar *message, gpointer user_data)
+{
+  got_error (domain, code, message);
 }
 
 static void
-parse_error(GibberXmppConnection *connection, gpointer user_data) {
+parse_error (GibberXmppConnection *connection, gpointer user_data)
+{
   fail ();
 }
 
 static void
-stream_opened(GibberXmppConnection *connection,
-              gchar *from, gchar *to, gchar *version, gpointer user_data) {
+stream_opened (GibberXmppConnection *connection,
+              gchar *from, gchar *to, gchar *version, gpointer user_data)
+{
   if (authenticated)
-    gibber_xmpp_connection_close(conn);
+    gibber_xmpp_connection_close (conn);
 }
 
 static void
-stream_closed(GibberXmppConnection *connection, gpointer user_data) {
+stream_closed (GibberXmppConnection *connection, gpointer user_data)
+{
   run_done = TRUE;
-  g_main_loop_quit(mainloop);
+  g_main_loop_quit (mainloop);
 }
 
 static void
-received_stanza(GibberXmppConnection *connection, GibberXmppStanza *stanza,
+received_stanza (GibberXmppConnection *connection, GibberXmppStanza *stanza,
    gpointer user_data)
 {
-  if (sasl == NULL) {
-    GError *error = NULL;
-    sasl = gibber_sasl_auth_new();
+  if (sasl == NULL)
+    {
+      GError *error = NULL;
+      sasl = gibber_sasl_auth_new ();
 
-    g_signal_connect(sasl, "username-requested",
-                      G_CALLBACK(return_str), (gpointer)username);
-    g_signal_connect(sasl, "password-requested",
-                      G_CALLBACK(return_str), (gpointer)password);
-    g_signal_connect(sasl, "authentication-succeeded",
-                      G_CALLBACK(auth_success), NULL);
-    g_signal_connect(sasl, "authentication-failed",
-                      G_CALLBACK(auth_failed), NULL);
+      g_signal_connect (sasl, "username-requested",
+          G_CALLBACK (return_str), (gpointer)username);
+      g_signal_connect (sasl, "password-requested",
+          G_CALLBACK (return_str), (gpointer)password);
+      g_signal_connect (sasl, "authentication-succeeded",
+          G_CALLBACK (auth_success), NULL);
+      g_signal_connect (sasl, "authentication-failed",
+          G_CALLBACK (auth_failed), NULL);
 
-    if (!gibber_sasl_auth_authenticate(sasl, servername,
-                                         connection, stanza,
-                                         current_test->allow_plain, &error)) {
-      got_error(error->domain, error->code, error->message);
-      g_error_free (error);
+      if (!gibber_sasl_auth_authenticate (sasl, servername, connection, stanza,
+          current_test->allow_plain, &error))
+        {
+          got_error (error->domain, error->code, error->message);
+          g_error_free (error);
+        }
     }
-  }
 }
 
 void
-run_rest(test_t *test) {
+run_rest (test_t *test)
+{
   TestSaslAuthServer *server;
 
-  servertransport = GIBBER_TRANSPORT(test_transport_new(send_hook, NULL));
+  servertransport = GIBBER_TRANSPORT(test_transport_new (send_hook, NULL));
 
-  server = test_sasl_auth_server_new(servertransport,
-                                     test->mech, username, password,
-                                     test->problem);
+  server = test_sasl_auth_server_new (servertransport, test->mech, username,
+      password, test->problem);
 
   authenticated = FALSE;
   run_done = FALSE;
   current_test = test;
 
-  clienttransport = GIBBER_TRANSPORT(test_transport_new(send_hook, NULL));
-  conn = gibber_xmpp_connection_new(clienttransport);
+  clienttransport = GIBBER_TRANSPORT (test_transport_new (send_hook, NULL));
+  conn = gibber_xmpp_connection_new (clienttransport);
 
-  g_signal_connect(conn, "parse-error",
-                   G_CALLBACK(parse_error), NULL);
-  g_signal_connect(conn, "stream-opened",
-                   G_CALLBACK(stream_opened), NULL);
-  g_signal_connect(conn, "stream-closed",
-                   G_CALLBACK(stream_closed), NULL);
-  g_signal_connect(conn, "received-stanza",
-                   G_CALLBACK(received_stanza), NULL);
-  gibber_xmpp_connection_open(conn, servername, NULL, "1.0");
+  g_signal_connect (conn, "parse-error", G_CALLBACK (parse_error), NULL);
+  g_signal_connect (conn, "stream-opened", G_CALLBACK (stream_opened), NULL);
+  g_signal_connect (conn, "stream-closed", G_CALLBACK (stream_closed), NULL);
+  g_signal_connect (conn, "received-stanza",
+      G_CALLBACK (received_stanza), NULL);
+  gibber_xmpp_connection_open (conn, servername, NULL, "1.0");
 
-  if (!run_done) {
-    g_main_loop_run(mainloop);
-  }
+  if (!run_done)
+    {
+      g_main_loop_run (mainloop);
+    }
 
-  if (sasl != NULL) {
-    g_object_unref(sasl);
-    sasl = NULL;
-  }
-  g_object_unref(servertransport);
-  g_object_unref(clienttransport);
-  g_object_unref(conn);
+  if (sasl != NULL)
+    {
+      g_object_unref (sasl);
+      sasl = NULL;
+    }
+
+  g_object_unref (servertransport);
+  g_object_unref (clienttransport);
+  g_object_unref (conn);
 
   fail_if (test->domain == 0 && error != NULL);
   fail_if (test->domain != 0 && error == NULL);
 
-  if (error != NULL) {
-    fail_if (test->domain != error->domain);
-    fail_if (test->code != error->code);
-  }
+  if (error != NULL)
+    {
+      fail_if (test->domain != error->domain);
+      fail_if (test->code != error->code);
+    }
 
   if (error != NULL)
-    g_error_free(error);
+    g_error_free (error);
+
   error = NULL;
 }
 
@@ -169,7 +180,8 @@ run_rest(test_t *test) {
 
 #define NUMBER_OF_TEST 6
 
-START_TEST (test_auth) {
+START_TEST (test_auth)
+{
   test_t tests[NUMBER_OF_TEST] = {
     SUCCESS("Normal authentication", NULL, TRUE),
     SUCCESS("Disallow PLAIN", "PLAIN", TRUE),
@@ -184,11 +196,11 @@ START_TEST (test_auth) {
        SERVER_PROBLEM_NO_SASL },
   };
 
-  g_type_init();
+  g_type_init ();
 
-  mainloop = g_main_loop_new(NULL, FALSE);
+  mainloop = g_main_loop_new (NULL, FALSE);
 
-  run_rest(&(tests[_i]));
+  run_rest (&(tests[_i]));
 } END_TEST
 
 TCase *
