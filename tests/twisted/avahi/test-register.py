75f01ff3be060e033b24d141b0ca824cb7f81c22
Sjoerd Simons <sjoerd.simons@collabora.co.uk>
2008-09-01 20:34:35 +0100

    Test that the service is register with the correct txt record

--- tests/twisted/avahi/test-register.py
+++ tests/twisted/avahi/test-register.py
@@ -1,8 +1,14 @@
 from saluttest import exec_test
 import avahitest
 from avahitest import AvahiListener
+from avahitest import txt_get_key
+from avahi import txt_array_to_string_array
 
 import time
+
+PUBLISHED_NAME="test-register"
+FIRST_NAME="lastname"
+LAST_NAME="lastname"
 
 def test(q, bus, conn):
     a = AvahiListener(q)
@@ -10,8 +16,21 @@ def test(q, bus, conn):
 
     conn.Connect()
 
-    q.expect('service-added',
-      name='test-register@' + avahitest.get_host_name())
+    e = q.expect('service-added',
+      name=PUBLISHED_NAME + "@" + avahitest.get_host_name())
+
+    service = a.resolver_for_service(e)
+
+    e = q.expect('service-resolved', service = service)
+
+    for (key, val) in { "1st": FIRST_NAME,
+                        "last": LAST_NAME,
+                        "status": "avail",
+                        "txtvers": "1" }.iteritems():
+        v =  txt_get_key(e.txt, key)
+        assert v == val, (key, val, v)
 
 if __name__ == '__main__':
-    exec_test(test, { "published-name": "test-register" })
+    exec_test(test, { "published-name": PUBLISHED_NAME,
+                      "first-name": FIRST_NAME,
+                      "last-name": LAST_NAME })
