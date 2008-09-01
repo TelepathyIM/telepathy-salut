4b90ac1708fcec38f55e594cd344e115c907714f
Sjoerd Simons <sjoerd.simons@collabora.co.uk>
2008-09-01 18:40:15 +0100

    Very basic first test: test that calling an avahi connection causes a service to appear

--- tests/twisted/avahi/test-register.py
+++ tests/twisted/avahi/test-register.py
@@ -1,7 +1,17 @@
 from saluttest import exec_test
+import avahitest
+from avahitest import AvahiListener
+
+import time
 
 def test(q, bus, conn):
-    pass
+    a = AvahiListener(q)
+    a.listen_for_service("_presence._tcp")
+
+    conn.Connect()
+
+    q.expect('service-added',
+      name='test-register@' + avahitest.get_host_name())
 
 if __name__ == '__main__':
-    exec_test(test)
+    exec_test(test, { "published-name": "test-register" })
