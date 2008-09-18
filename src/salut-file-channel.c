57acdaab94f65c3b42ab476cd4dd2a6ae328df40
Jonny Lamb <jonny.lamb@collabora.co.uk>
2008-09-18 15:52:31 +0100

    Fixed transferred-bytes write access.
    Signed-off-by: Jonny Lamb <jonny.lamb@collabora.co.uk>

--- src/salut-file-channel.c
+++ src/salut-file-channel.c
@@ -572,7 +572,7 @@ salut_file_channel_class_init (SalutFileChannelClass *salut_file_channel_class)
       0,
       G_MAXUINT64,
       0,
-      G_PARAM_READWRITE |
+      G_PARAM_READABLE |
       G_PARAM_STATIC_NICK |
       G_PARAM_STATIC_BLURB);
   g_object_class_install_property (object_class, PROP_TRANSFERRED_BYTES, param_spec);
