134bbc3a9cdc9b1a0a86c4d467a4c8acd4e0385a
Alban Crequy <alban.crequy@collabora.co.uk>
2008-10-08 15:48:33 +0100

    lib/gibber/gibber-namespaces.h: Remove all the uneeded casts

--- lib/gibber/gibber-namespaces.h
+++ lib/gibber/gibber-namespaces.h
@@ -3,64 +3,64 @@
 #define __GIBBER_NAMESPACES_H__
 
 #define GIBBER_XMPP_NS_STREAM \
-  (const gchar *)"http://etherx.jabber.org/streams"
+  "http://etherx.jabber.org/streams"
 
 #define GIBBER_XMPP_NS_TLS \
-  (const gchar *)"urn:ietf:params:xml:ns:xmpp-tls"
+  "urn:ietf:params:xml:ns:xmpp-tls"
 
 #define GIBBER_XMPP_NS_SASL_AUTH \
-  (const gchar *)"urn:ietf:params:xml:ns:xmpp-sasl"
+  "urn:ietf:params:xml:ns:xmpp-sasl"
 
 #define NS_DISCO_INFO \
-  (const gchar *)"http://jabber.org/protocol/disco#info"
+  "http://jabber.org/protocol/disco#info"
 
 #define NS_DISCO_ITEMS \
-  (const gchar *)"http://jabber.org/protocol/disco#items"
+  "http://jabber.org/protocol/disco#items"
 
 #define GIBBER_XMPP_NS_XHTML_IM \
-  (const gchar *)"http://jabber.org/protocol/xhtml-im"
+  "http://jabber.org/protocol/xhtml-im"
 
 #define GIBBER_XMPP_NS_IBB \
-  (const gchar *)"http://jabber.org/protocol/ibb"
+  "http://jabber.org/protocol/ibb"
 
 #define GIBBER_XMPP_NS_AMP \
-  (const gchar *)"http://jabber.org/protocol/amp"
+  "http://jabber.org/protocol/amp"
 
 #define GIBBER_W3C_NS_XHTML \
-  (const gchar *)"http://www.w3.org/1999/xhtml"
+  "http://www.w3.org/1999/xhtml"
 
 #define GIBBER_TELEPATHY_NS_CAPS \
-  (const gchar *)"http://telepathy.freedesktop.org/caps"
+  "http://telepathy.freedesktop.org/caps"
 
 #define GIBBER_TELEPATHY_NS_TUBES \
-  (const gchar *)"http://telepathy.freedesktop.org/xmpp/tubes"
+  "http://telepathy.freedesktop.org/xmpp/tubes"
 
 #define GIBBER_TELEPATHY_NS_OLPC_ACTIVITY_PROPS \
-  (const gchar *)"http://laptop.org/xmpp/activity-properties"
+  "http://laptop.org/xmpp/activity-properties"
 
 #define GIBBER_XMPP_NS_SI \
-  (const gchar *)"http://jabber.org/protocol/si"
+  "http://jabber.org/protocol/si"
 
 #define GIBBER_XMPP_NS_FEATURENEG \
-  (const gchar *)"http://jabber.org/protocol/feature-neg"
+  "http://jabber.org/protocol/feature-neg"
 
 #define GIBBER_XMPP_NS_DATA \
-  (const gchar *)"jabber:x:data"
+  "jabber:x:data"
 
 #define GIBBER_XMPP_NS_EVENT \
   (const gchar *)"jabber:x:event"
 
 #define GIBBER_XMPP_NS_STANZAS \
-  (const gchar *)"urn:ietf:params:xml:ns:xmpp-stanzas"
+  "urn:ietf:params:xml:ns:xmpp-stanzas"
 
 #define GIBBER_XMPP_NS_OOB \
-  (const gchar *)"jabber:iq:oob"
+  "jabber:iq:oob"
 
 #define GIBBER_XMPP_NS_JINGLE_ERRORS \
-  (const gchar *)"http://jabber.org/protocol/jingle#errors"
+  "http://jabber.org/protocol/jingle#errors"
 
 #define GIBBER_TELEPATHY_NS_CLIQUE \
-  (const gchar *)"http://telepathy.freedesktop.org/xmpp/clique"
+  "http://telepathy.freedesktop.org/xmpp/clique"
 
 #define GIBBER_XMPP_NS_OOB \
   (const gchar *)"jabber:iq:oob"
