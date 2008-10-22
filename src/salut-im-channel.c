ba192075fdd28c8f5da9788aafe2bd5b1ed0b668
Alban Crequy <alban.crequy@collabora.co.uk>
2008-10-22 14:20:28 -0400

    SalutImChannel: Requested, InitiatorID, InitiatorHandle are no more future properties

--- src/salut-im-channel.c
+++ src/salut-im-channel.c
@@ -285,9 +285,9 @@ salut_im_channel_get_property (GObject *object,
                 TP_IFACE_CHANNEL, "TargetHandleType",
                 TP_IFACE_CHANNEL, "ChannelType",
                 TP_IFACE_CHANNEL, "TargetID",
-                SALUT_IFACE_CHANNEL_FUTURE, "InitiatorHandle",
-                SALUT_IFACE_CHANNEL_FUTURE, "InitiatorID",
-                SALUT_IFACE_CHANNEL_FUTURE, "Requested",
+                TP_IFACE_CHANNEL, "InitiatorHandle",
+                TP_IFACE_CHANNEL, "InitiatorID",
+                TP_IFACE_CHANNEL, "Requested",
                 NULL));
         break;
       default:
@@ -407,9 +407,6 @@ salut_im_channel_class_init (SalutImChannelClass *salut_im_channel_class)
       { "TargetID", "target-id", NULL },
       { "ChannelType", "channel-type", NULL },
       { "Interfaces", "interfaces", NULL },
-      { NULL }
-  };
-  static TpDBusPropertiesMixinPropImpl future_props[] = {
       { "Requested", "requested", NULL },
       { "InitiatorHandle", "initiator-handle", NULL },
       { "InitiatorID", "initiator-id", NULL },
@@ -420,11 +417,6 @@ salut_im_channel_class_init (SalutImChannelClass *salut_im_channel_class)
         tp_dbus_properties_mixin_getter_gobject_properties,
         NULL,
         channel_props,
-      },
-      { SALUT_IFACE_CHANNEL_FUTURE,
-        tp_dbus_properties_mixin_getter_gobject_properties,
-        NULL,
-        future_props,
       },
       { NULL }
   };
