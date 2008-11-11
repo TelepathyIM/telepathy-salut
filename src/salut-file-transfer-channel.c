2eaba61207958fbc940f74e503b21856802968ff
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2008-11-11 16:58:43 +0000

    coding style tweaks

--- src/salut-file-transfer-channel.c
+++ src/salut-file-transfer-channel.c
@@ -57,7 +57,8 @@ channel_iface_init (gpointer g_iface, gpointer iface_data);
 static void
 file_transfer_iface_init (gpointer g_iface, gpointer iface_data);
 
-G_DEFINE_TYPE_WITH_CODE (SalutFileTransferChannel, salut_file_transfer_channel, G_TYPE_OBJECT,
+G_DEFINE_TYPE_WITH_CODE (SalutFileTransferChannel, salut_file_transfer_channel,
+    G_TYPE_OBJECT,
     G_IMPLEMENT_INTERFACE (TP_TYPE_SVC_CHANNEL, channel_iface_init);
     G_IMPLEMENT_INTERFACE (TP_TYPE_SVC_DBUS_PROPERTIES,
                            tp_dbus_properties_mixin_iface_init);
@@ -779,7 +780,7 @@ salut_file_transfer_channel_finalize (GObject *object)
  */
 static void
 salut_file_transfer_channel_close (TpSvcChannel *iface,
-                          DBusGMethodInvocation *context)
+                                   DBusGMethodInvocation *context)
 {
   SalutFileTransferChannel *self = SALUT_FILE_TRANSFER_CHANNEL (iface);
 
