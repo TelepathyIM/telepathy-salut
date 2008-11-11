a12080ee18359f7e832f7aef7b1cd9aac5d817ef
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2008-11-11 17:13:49 +0000

    gibber-oob-file-transfer: prefix HTTP status code enum with HTTP_STATUS_CODE_. Add the OK code

--- lib/gibber/gibber-oob-file-transfer.c
+++ lib/gibber/gibber-oob-file-transfer.c
@@ -36,8 +36,9 @@
 #include "gibber-debug.h"
 
 enum {
-  NOT_FOUND = 404,
-  NOT_ACCEPTABLE = 406
+  HTTP_STATUS_CODE_OK = 200,
+  HTTP_STATUS_CODE_NOT_FOUND = 404,
+  HTTP_STATUS_CODE_NOT_ACCEPTABLE = 406
 };
 
 G_DEFINE_TYPE(GibberOobFileTransfer, gibber_oob_file_transfer,
@@ -261,7 +262,7 @@ http_client_chunk_cb (SoupMessage *msg,
   g_io_channel_write_chars (self->priv->channel, msg->response.body,
       msg->response.length, NULL, NULL);
 
-  if (msg->status_code != 200)
+  if (msg->status_code != HTTP_STATUS_CODE_OK)
     {
       /* Something did wrong, so it's not file data. Don't fire the
        * transferred-chunk signal. */
@@ -304,7 +305,7 @@ http_client_finished_chunks_cb (SoupMessage *msg,
 
   DEBUG ("Finished HTTP chunked file transfer");
 
-  if (msg->status_code != 200)
+  if (msg->status_code != HTTP_STATUS_CODE_OK)
     {
       GError *error = NULL;
       const gchar *reason_phrase;
@@ -670,13 +671,13 @@ gibber_oob_file_transfer_cancel (GibberFileTransfer *ft,
 
   switch (error_code)
     {
-      case NOT_FOUND:
+      case HTTP_STATUS_CODE_NOT_FOUND:
         gibber_xmpp_node_set_attribute (error_node, "code", code_string);
         gibber_xmpp_node_set_attribute (error_node, "type", "cancel");
         error_desc = gibber_xmpp_node_add_child_ns (error_node,
             "item-not-found", GIBBER_XMPP_NS_STANZAS);
         break;
-      case NOT_ACCEPTABLE:
+      case HTTP_STATUS_CODE_NOT_ACCEPTABLE:
         gibber_xmpp_node_set_attribute (error_node, "code", code_string);
         gibber_xmpp_node_set_attribute (error_node, "type", "modify");
         error_desc = gibber_xmpp_node_add_child_ns (error_node,
