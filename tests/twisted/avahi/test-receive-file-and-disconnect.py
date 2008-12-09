8e6de42447c2c22628312e8266c7b0228973a999
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2008-12-09 14:05:42 +0000

    add test-receive-file-and-disconnect.py

---
+++ tests/twisted/avahi/test-receive-file-and-disconnect.py
@@ -0,0 +1,18 @@
+import socket
+
+from saluttest import exec_test
+from file_transfer_helper import ReceiveFileTest
+
+class ReceiveFileAndDisconnectTest(ReceiveFileTest):
+    def receive_file(self):
+        s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        s.connect(self.address)
+
+        # disconnect
+        self.conn.Disconnect()
+        self.q.expect('dbus-signal', signal='StatusChanged', args=[2, 1])
+        return True
+
+if __name__ == '__main__':
+    test = ReceiveFileAndDisconnectTest()
+    exec_test(test.test)
