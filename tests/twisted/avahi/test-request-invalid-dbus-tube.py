50a8650046f76cb41c525b56cabf93c798f2ef05
Will Thompson <will.thompson@collabora.co.uk>
2009-01-10 19:56:40 +0000

    Add more invalid services names to dtube request test

--- tests/twisted/avahi/test-request-invalid-dbus-tube.py
+++ tests/twisted/avahi/test-request-invalid-dbus-tube.py
@@ -18,7 +18,15 @@ from twisted.internet import reactor
 PUBLISHED_NAME="test-tube"
 
 CHANNEL_TYPE_TUBES = "org.freedesktop.Telepathy.Channel.Type.Tubes"
+C_T_DTUBE = 'org.freedesktop.Telepathy.Channel.Type.DBusTube.DRAFT'
 HT_CONTACT = 1
+
+invalid_service_names = [ 'invalidServiceName'
+                        , 'one ten hundred thousand million'
+                        , 'me.is.it.you?.hello.you.sexy.sons.o.@#$%.heh'
+                        , ':1.1'
+                        , ''
+                        ]
 
 def test(q, bus, conn):
     conn.Connect()
@@ -27,21 +35,24 @@ def test(q, bus, conn):
     requestotron = dbus.Interface(conn,
             'org.freedesktop.Telepathy.Connection.Interface.Requests')
 
-    try:
-        requestotron.CreateChannel(
-            {'org.freedesktop.Telepathy.Channel.ChannelType':
-                'org.freedesktop.Telepathy.Channel.Type.DBusTube.DRAFT',
-             'org.freedesktop.Telepathy.Channel.TargetHandleType':
-                 HT_CONTACT,
-             'org.freedesktop.Telepathy.Channel.TargetID':
-                 'alice',
-             'org.freedesktop.Telepathy.Channel.Type.DBusTube.DRAFT.ServiceName':
-                'invalidServiceName'
-            });
-    except dbus.DBusException, e:
-        assert e.get_dbus_name() == 'org.freedesktop.Telepathy.Errors.InvalidArgument'
-    else:
-        assert False
+    for invalid_service_name in invalid_service_names:
+        try:
+            requestotron.CreateChannel(
+                {'org.freedesktop.Telepathy.Channel.ChannelType':
+                     C_T_DTUBE,
+                 'org.freedesktop.Telepathy.Channel.TargetHandleType':
+                     HT_CONTACT,
+                 'org.freedesktop.Telepathy.Channel.TargetID':
+                     'alice',
+                 C_T_DTUBE + '.ServiceName':
+                    invalid_service_name
+                });
+        except dbus.DBusException, e:
+            assert e.get_dbus_name() == \
+                'org.freedesktop.Telepathy.Errors.InvalidArgument', \
+                (e.get_dbus_name(), invalid_service_name)
+        else:
+            assert False
 
 if __name__ == '__main__':
     exec_test(test)
