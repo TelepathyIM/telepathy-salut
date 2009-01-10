e282668dc4f805e1090158f1a17d2cf2fc041b4c
Will Thompson <will.thompson@collabora.co.uk>
2009-01-10 19:15:40 +0000

    Removed unused variables in test-request-invalid-dbus-tube

--- tests/twisted/avahi/test-request-invalid-dbus-tube.py
+++ tests/twisted/avahi/test-request-invalid-dbus-tube.py
@@ -19,22 +19,6 @@ PUBLISHED_NAME="test-tube"
 
 CHANNEL_TYPE_TUBES = "org.freedesktop.Telepathy.Channel.Type.Tubes"
 HT_CONTACT = 1
-HT_ROOM = 2
-HT_CONTACT_LIST = 3
-TEXT_MESSAGE_TYPE_NORMAL = dbus.UInt32(0)
-SOCKET_ADDRESS_TYPE_UNIX = dbus.UInt32(0)
-SOCKET_ADDRESS_TYPE_IPV4 = dbus.UInt32(2)
-SOCKET_ADDRESS_TYPE_IPV6 = dbus.UInt32(3)
-SOCKET_ACCESS_CONTROL_LOCALHOST = dbus.UInt32(0)
-
-sample_parameters = dbus.Dictionary({
-    's': 'hello',
-    'ay': dbus.ByteArray('hello'),
-    'u': dbus.UInt32(123),
-    'i': dbus.Int32(-123),
-    }, signature='sv')
-
-test_string = "This string travels on a tube !"
 
 def test(q, bus, conn):
     conn.Connect()
