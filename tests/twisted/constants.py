61cb83a59d525cac7fcd3dab115011e6c1eff42b
Will Thompson <will.thompson@collabora.co.uk>
2009-01-11 00:42:18 +0000

    Add a small set of test constants

---
+++ tests/twisted/constants.py
@@ -0,0 +1,21 @@
+"""
+Some handy constants for other tests to share and enjoy.
+"""
+
+HT_CONTACT = 1
+
+CHANNEL = "org.freedesktop.Telepathy.Channel"
+CHANNEL_TYPE_TUBES = CHANNEL + ".Type.Tubes"
+CHANNEL_TYPE_STREAM_TUBE = CHANNEL + ".Type.StreamTube.DRAFT"
+
+CHANNEL_TYPE = CHANNEL + '.ChannelType'
+TARGET_HANDLE_TYPE = CHANNEL + '.TargetHandleType'
+TARGET_HANDLE = CHANNEL + '.TargetHandle'
+
+CONN = "org.freedesktop.Telepathy.Connection"
+CONN_IFACE_REQUESTS = CONN + '.Interface.Requests'
+
+ERRORS = 'org.freedesktop.Telepathy.Errors'
+NOT_AVAILABLE = ERRORS + '.NotAvailable'
+INVALID_ARGUMENT = ERRORS + '.InvalidArgument'
+
