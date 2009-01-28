1e6165759dd24c021eeb127c9630929cffcab865
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2009-01-28 15:50:17 +0000

    constants: fix INTERFACES

--- tests/twisted/constants.py
+++ tests/twisted/constants.py
@@ -23,7 +23,7 @@ TARGET_ID = CHANNEL + '.TargetID'
 REQUESTED = CHANNEL + '.Requested'
 INITIATOR_HANDLE = CHANNEL + '.InitiatorHandle'
 INITIATOR_ID = CHANNEL + '.InitiatorID'
-INTERFACES = CHANNEL + '.org.freedesktop.Telepathy.Channel.Interfaces'
+INTERFACES = CHANNEL + '.Interfaces'
 
 CONN = "org.freedesktop.Telepathy.Connection"
 CONN_IFACE_REQUESTS = CONN + '.Interface.Requests'
