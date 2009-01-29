689abbf2b202de240a04b93b6ab8f1bfc431b3d0
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2009-01-29 15:20:59 +0000

    salut_tube_stream_accepted: fire TubeChannelStateChanged signal

--- src/tube-stream.c
+++ src/tube-stream.c
@@ -1832,6 +1832,9 @@ salut_tube_stream_accepted (SalutTubeIface *tube)
 
   priv->state = TP_TUBE_STATE_OPEN;
   g_signal_emit (G_OBJECT (self), signals[OPENED], 0);
+
+  salut_svc_channel_interface_tube_emit_tube_channel_state_changed (
+      self, SALUT_TUBE_CHANNEL_STATE_OPEN);
 }
 
 /**
