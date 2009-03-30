086e98897a958c573eafec569ff56e95c8d54ec0
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2009-03-30 17:55:15 +0100

    caps_helper: add check_caps

--- tests/twisted/avahi/caps_helper.py
+++ tests/twisted/avahi/caps_helper.py
@@ -1,6 +1,9 @@
 import hashlib
 import base64
 
+from avahitest import txt_get_key
+import ns
+
 def compute_caps_hash(identities, features, dataforms):
     S = ''
 
@@ -15,6 +18,20 @@ def compute_caps_hash(identities, features, dataforms):
     m = hashlib.sha1()
     m.update(S)
     return base64.b64encode(m.digest())
+
+def check_caps(txt, ver):
+    for (key, val) in { "1st": "test",
+                        "last": "suite",
+                        "status": "avail",
+                        "txtvers": "1" }.iteritems():
+        v =  txt_get_key(txt, key)
+        assert v == val, (key, val, v)
+
+    assert txt_get_key(txt, "hash") == "sha-1"
+    assert txt_get_key(txt, "node") == ns.TELEPATHY_CAPS
+
+    v = txt_get_key(txt, "ver")
+    assert v == ver, (v, ver)
 
 if __name__ == '__main__':
     # example from XEP-0115
