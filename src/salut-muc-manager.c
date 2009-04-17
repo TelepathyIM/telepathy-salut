5979dfce0cb08612e5c84c9c2b9aa1032989ff61
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2009-04-17 15:07:52 +0100

    identation fix

--- src/salut-muc-manager.c
+++ src/salut-muc-manager.c
@@ -332,17 +332,17 @@ salut_muc_manager_foreach_channel (TpChannelManager *iface,
 
   g_hash_table_iter_init (&iter, priv->tubes_channels);
   while (g_hash_table_iter_next (&iter, NULL, &value))
-  {
-    TpExportableChannel *chan = TP_EXPORTABLE_CHANNEL (value);
+    {
+      TpExportableChannel *chan = TP_EXPORTABLE_CHANNEL (value);
 
-    /* Add channels of type Channel.Type.Tubes */
-    foreach (chan, user_data);
+      /* Add channels of type Channel.Type.Tubes */
+      foreach (chan, user_data);
 
-    /* Add channels of type Channel.Type.{Stream|DBus}Tube which live in the
-     * SalutTubesChannel object */
-    salut_tubes_channel_foreach (SALUT_TUBES_CHANNEL (chan), foreach,
-        user_data);
-  }
+      /* Add channels of type Channel.Type.{Stream|DBus}Tube which live in the
+       * SalutTubesChannel object */
+      salut_tubes_channel_foreach (SALUT_TUBES_CHANNEL (chan), foreach,
+          user_data);
+    }
 }
 
 static const gchar * const muc_channel_fixed_properties[] = {
