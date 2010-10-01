6053b767827fb68d06ef32255e0cd985815dcb0d
Simon McVittie <simon.mcvittie@collabora.co.uk>
2010-10-01 14:11:49 +0100

    saluttest: use install_colourer() from servicetest

--- tests/twisted/saluttest.py
+++ tests/twisted/saluttest.py
@@ -50,36 +50,11 @@ def make_connection(bus, event_func, params=None):
     return servicetest.make_connection(bus, event_func, 'salut',
         'local-xmpp', default_params)
 
-def install_colourer():
-    def red(s):
-        return '\x1b[31m%s\x1b[0m' % s
-
-    def green(s):
-        return '\x1b[32m%s\x1b[0m' % s
-
-    patterns = {
-        'handled': green,
-        'not handled': red,
-        }
-
-    class Colourer:
-        def __init__(self, fh, patterns):
-            self.fh = fh
-            self.patterns = patterns
-
-        def write(self, s):
-            f = self.patterns.get(s, lambda x: x)
-            self.fh.write(f(s))
-
-    sys.stdout = Colourer(sys.stdout, patterns)
-    return sys.stdout
-
-
 def exec_test_deferred (fun, params, protocol=None, timeout=None):
     colourer = None
 
-    if sys.stdout.isatty():
-        colourer = install_colourer()
+    if sys.stdout.isatty() or 'CHECK_FORCE_COLOR' in os.environ:
+        colourer = servicetest.install_colourer()
 
     queue = servicetest.IteratingEventQueue(timeout)
     queue.verbose = (
