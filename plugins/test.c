6195e787e6fb2c92cebe85ebd815210f0952a2f0
Jonny Lamb <jonny.lamb@collabora.co.uk>
2011-02-07 11:49:59 +0000

    plugins: add a test plugin
    Signed-off-by: Jonny Lamb <jonny.lamb@collabora.co.uk>

---
+++ plugins/test.c
@@ -0,0 +1,41 @@
+#include "test.h"
+
+#include <salut/plugin.h>
+
+#define DEBUG(msg, ...) \
+  g_debug ("%s: " msg, G_STRFUNC, ##__VA_ARGS__)
+
+static void plugin_iface_init (
+    gpointer g_iface,
+    gpointer data);
+
+G_DEFINE_TYPE_WITH_CODE (TestPlugin, test_plugin, G_TYPE_OBJECT,
+    G_IMPLEMENT_INTERFACE (SALUT_TYPE_PLUGIN, plugin_iface_init);
+    )
+
+static void
+test_plugin_init (TestPlugin *object)
+{
+  DEBUG ("%p", object);
+}
+
+static void
+test_plugin_class_init (TestPluginClass *klass)
+{
+}
+
+static void
+plugin_iface_init (
+    gpointer g_iface,
+    gpointer data G_GNUC_UNUSED)
+{
+  SalutPluginInterface *iface = g_iface;
+
+  iface->name = "Salut test plugin";
+}
+
+SalutPlugin *
+salut_plugin_create ()
+{
+  return g_object_new (test_plugin_get_type (), NULL);
+}
