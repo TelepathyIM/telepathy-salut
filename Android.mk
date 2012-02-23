a842971ca3c1aa11c3495fd88ddcad89eee321c0
Alvaro Soliverez <alvaro.soliverez@collabora.co.uk>
2012-02-23 00:19:12 -0300

    Fix Android build after plugin library refactor

--- Android.mk
+++ Android.mk
@@ -18,7 +18,7 @@ telepathy-salut-configure-real:
 	PKG_CONFIG_LIBDIR=$(CONFIGURE_PKG_CONFIG_LIBDIR) \
 	PKG_CONFIG_TOP_BUILD_DIR=$(PKG_CONFIG_TOP_BUILD_DIR) \
 	$(TELEPATHY_SALUT_TOP)/$(CONFIGURE) --host=arm-linux-androideabi \
-	        --disable-submodules \
+        --disable-submodules --disable-python --disable-avahi-tests \
 		--disable-Werror && \
 	for file in $(TELEPATHY_SALUT_BUILT_SOURCES); do \
 		rm -f $$file && \
