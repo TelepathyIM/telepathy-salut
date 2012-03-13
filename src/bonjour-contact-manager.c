0569559273adfa2f8fb17454aad8ca086d327fc8
Siraj Razick <siraj.razick@collabora.co.uk>
2012-03-13 16:27:40 -0400

    bonjour-contact-manager: Start browsing for bonjour service
    Patch to enable Bonjour Browsing, and start looking for near by
    computers.

--- src/bonjour-contact-manager.c
+++ src/bonjour-contact-manager.c
@@ -22,10 +22,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <dns_sd.h>
+#undef interface
+
 #define DEBUG_FLAG DEBUG_MUC
 #include "debug.h"
 
+#include "bonjour-contact.h"
 #include "bonjour-contact-manager.h"
+#include "plugin-connection.h"
+#include "connection.h"
 
 G_DEFINE_TYPE (SalutBonjourContactManager, salut_bonjour_contact_manager,
     SALUT_TYPE_CONTACT_MANAGER);
@@ -43,7 +49,9 @@ typedef struct _SalutBonjourContactManagerPrivate SalutBonjourContactManagerPriv
 struct _SalutBonjourContactManagerPrivate
 {
   SalutBonjourDiscoveryClient *discovery_client;
+  DNSServiceRef presence_browser;
 
+  gboolean all_for_now;
   gboolean dispose_has_run;
 };
 
@@ -101,6 +109,7 @@ salut_bonjour_contact_manager_init (SalutBonjourContactManager *self)
 
   self->priv = priv;
 
+  priv->all_for_now = FALSE;
   priv->discovery_client = NULL;
 }
 
@@ -108,13 +117,129 @@ static SalutContact *
 salut_bonjour_contact_manager_create_contact (SalutContactManager *mgr,
                                               const gchar *name)
 {
-  return NULL;
+  SalutBonjourContactManager *self = SALUT_BONJOUR_CONTACT_MANAGER (mgr);
+  SalutBonjourContactManagerPrivate *priv =
+    SALUT_BONJOUR_CONTACT_MANAGER_GET_PRIVATE (self);
+
+  return SALUT_CONTACT (salut_bonjour_contact_new (mgr->connection,
+        name, priv->discovery_client));
+}
+
+static void DNSSD_API
+_salut_bonjour_service_browse_cb (DNSServiceRef service,
+                                  DNSServiceFlags flags,
+                                  uint32_t interfaceIndex,
+                                  DNSServiceErrorType error_type,
+                                  const char *name,
+                                  const char *regtype,
+                                  const char *domain,
+                                  void *context)
+{
+  SalutBonjourContactManager *self = SALUT_BONJOUR_CONTACT_MANAGER (context);
+  SalutContactManager *mgr = SALUT_CONTACT_MANAGER (self);
+  SalutBonjourContactManagerPrivate *priv =
+    SALUT_BONJOUR_CONTACT_MANAGER_GET_PRIVATE (self);
+  SalutContact *contact;
+  const char *self_contact_name;
+
+  if (error_type != kDNSServiceErr_NoError)
+    {
+      DEBUG ("Browser Failed with : (%d)", error_type);
+      salut_bonjour_discovery_client_drop_svc_ref (priv->discovery_client,
+          &priv->presence_browser);
+      return;
+    }
+
+  self_contact_name = salut_connection_get_name (SALUT_PLUGIN_CONNECTION (
+        mgr->connection));
+
+  if (g_ascii_strcasecmp (name, self_contact_name) == 0)
+    return;
+
+  if (flags & kDNSServiceFlagsAdd)
+    {
+      DEBUG ("New Service : %s, on iface : %u, with domain %s of type %s",
+          name, interfaceIndex, domain, regtype);
+
+      contact = g_hash_table_lookup (mgr->contacts, name);
+
+      if (contact == NULL)
+        {
+          contact = salut_bonjour_contact_manager_create_contact (mgr, name);
+          salut_contact_manager_contact_created (mgr, contact);
+        }
+      else if (!salut_bonjour_contact_has_services (SALUT_BONJOUR_CONTACT
+            (contact)))
+        {
+          g_object_ref (contact);
+        }
+
+      if (!salut_bonjour_contact_add_service (SALUT_BONJOUR_CONTACT (contact),
+            interfaceIndex, name, regtype, domain))
+        {
+          /* If we couldn't add the server check the refcounting */
+          if (!salut_bonjour_contact_has_services (SALUT_BONJOUR_CONTACT (contact)))
+            g_object_unref (contact);
+        }
+      else
+        {
+          WockyContactFactory *contact_factory;
+
+          contact_factory = wocky_session_get_contact_factory (
+              mgr->connection->session);
+          wocky_contact_factory_add_ll_contact (contact_factory,
+              WOCKY_LL_CONTACT (contact));
+        }
+    }
+  else
+    {
+      DEBUG ("Contact Removed : %s", name);
+      contact = g_hash_table_lookup (mgr->contacts, name);
+
+      if (contact != NULL)
+        {
+          salut_bonjour_contact_remove_service (SALUT_BONJOUR_CONTACT (contact),
+              interfaceIndex, name, regtype, domain);
+          g_object_unref (contact);
+        }
+      else
+        {
+          DEBUG ("Unknown Contact Removed from Service Browser");
+        }
+    }
+
+  if (!priv->all_for_now && !(flags & kDNSServiceFlagsMoreComing))
+    {
+      g_signal_emit_by_name (self, "all-for-now");
+      priv->all_for_now = TRUE;
+    }
 }
 
 static gboolean
 salut_bonjour_contact_manager_start (SalutContactManager *mgr,
                                      GError **error)
 {
+  SalutBonjourContactManager *self = SALUT_BONJOUR_CONTACT_MANAGER (mgr);
+  SalutBonjourContactManagerPrivate *priv =
+    SALUT_BONJOUR_CONTACT_MANAGER_GET_PRIVATE (self);
+  DNSServiceErrorType error_type = kDNSServiceErr_NoError;
+  const gchar *dnssd_name =
+    salut_bonjour_discovery_client_get_dnssd_name (priv->discovery_client);
+
+  error_type = DNSServiceBrowse (&priv->presence_browser, 0,
+      kDNSServiceInterfaceIndexAny, dnssd_name, NULL,
+      _salut_bonjour_service_browse_cb, self);
+
+  if (error_type != kDNSServiceErr_NoError)
+    {
+      *error = g_error_new (TP_ERRORS, TP_ERROR_NOT_AVAILABLE,
+          "Service Browse Failed with : (%d)", error_type);
+      return FALSE;
+    }
+
+  salut_bonjour_discovery_client_watch_svc_ref (priv->discovery_client,
+      &priv->presence_browser);
+
   return TRUE;
 }
 
@@ -122,6 +247,11 @@ static void
 salut_bonjour_contact_manager_dispose_contact (SalutContactManager *mgr,
                                                SalutContact *contact)
 {
+  if (salut_bonjour_contact_has_services (SALUT_BONJOUR_CONTACT (contact)))
+    {
+      /* We reffed this contact as it has services */
+      g_object_unref (contact);
+    }
 }
 
 static void
@@ -130,6 +260,9 @@ salut_bonjour_contact_manager_close_all (SalutContactManager *mgr)
   SalutBonjourContactManager *self = SALUT_BONJOUR_CONTACT_MANAGER (mgr);
   SalutBonjourContactManagerPrivate *priv =
     SALUT_BONJOUR_CONTACT_MANAGER_GET_PRIVATE (self);
+
+  salut_bonjour_discovery_client_drop_svc_ref (priv->discovery_client,
+      &priv->presence_browser);
 
   if (priv->discovery_client != NULL)
     {
@@ -141,10 +274,6 @@ salut_bonjour_contact_manager_close_all (SalutContactManager *mgr)
 static void
 salut_bonjour_contact_manager_constructed (GObject *object)
 {
-  SalutBonjourContactManager *self = SALUT_BONJOUR_CONTACT_MANAGER (object);
-  SalutBonjourContactManagerPrivate *priv =
-    SALUT_BONJOUR_CONTACT_MANAGER_GET_PRIVATE (self);
-
   if (G_OBJECT_CLASS (salut_bonjour_contact_manager_parent_class)->constructed)
     G_OBJECT_CLASS (salut_bonjour_contact_manager_parent_class)->constructed (object);
 }
