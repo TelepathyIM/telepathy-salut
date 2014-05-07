05102dc3123b2368503fc7ca610a1d262e6df4f8
Simon McVittie <simon.mcvittie@collabora.co.uk>
2014-05-07 11:27:36 +0100

    Use Gabble's autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,39 +1,25 @@
 #!/bin/sh
 set -e
 
-if test -n "$AUTOMAKE"; then
-    : # don't override an explicit user request
-elif automake-1.11 --version >/dev/null 2>/dev/null && \
-     aclocal-1.11 --version >/dev/null 2>/dev/null; then
-    # If we have automake-1.11, use it. This is the oldest version (=> least
-    # likely to introduce undeclared dependencies) that will give us
-    # --enable-silent-rules support.
-    AUTOMAKE=automake-1.11
-    export AUTOMAKE
-    ACLOCAL=aclocal-1.11
-    export ACLOCAL
-fi
-
 autoreconf -i -f
 
-#Check if building submodules
-build_submodules=true
+#Check if submodules should be enabled
+enable_submodules=true
 for arg in $*; do
     case $arg in
-    --disable-submodules)
-    build_submodules=false
-    ;;
-*)
-;;
-esac
+        --disable-submodules)
+            enable_submodules=false
+            ;;
+        *)
+            ;;
+    esac
 done
 
-if test $build_submodules = true; then
-    # Fetch Wocky if needed
-    if test ! -f lib/ext/wocky/autogen.sh;
-    then
-    echo "+ Setting up Wocky submodule"
-    git submodule init
+if test $enable_submodules = true; then
+    # Fetch submodules if needed
+    if test ! -f lib/ext/wocky/autogen.sh; then
+        echo "+ Setting up submodules"
+        git submodule init
     fi
     git submodule update
 
@@ -43,16 +29,21 @@ if test $build_submodules = true; then
     cd ../../..
 fi
 
-run_configure=true
-for arg in $*; do
-    case $arg in
-        --no-configure)
-            run_configure=false
-            ;;
-        *)
-            ;;
-    esac
-done
+# Honor NOCONFIGURE for compatibility with gnome-autogen.sh
+if test x"$NOCONFIGURE" = x; then
+    run_configure=true
+    for arg in $*; do
+        case $arg in
+            --no-configure)
+                run_configure=false
+                ;;
+            *)
+                ;;
+        esac
+    done
+else
+    run_configure=false
+fi
 
 if test $run_configure = true; then
     ./configure "$@"
